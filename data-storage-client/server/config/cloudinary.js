ce3ee0b9b643ccbaaf9df4bf4a0a72e4da1a5e7a
Guresh07 <challaguresh1234@gmail.com>
2025-06-13 16:14:05 +0530

    frontend

--- data-storage-client/server/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: "dvazt1hyg",
-  api_key: "851468499428823",
-  api_secret: "OYdTuy17YgBzXNbVdRiLHz51PIc",
-});
-
-module.exports = cloudinary;
