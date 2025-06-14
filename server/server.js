82eb6397afe7303a78a6186896e200d690433aac
Guresh07 <160294259+Guresh07@users.noreply.github.com>
2025-06-14 14:45:40 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(mongodb+srv://Ruthul:Ruthul1234@cluster0.qpx6zig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
+mongoose.connect("mongodb+srv://Ruthul:Ruthul1234@cluster0.qpx6zig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error: ", err));
 
