2be263d62e0574a148fe5ee24f0c15b386622b73
Guresh07 <160294259+Guresh07@users.noreply.github.com>
2025-06-14 11:53:32 +0530

    Update api.js

--- data-storage-client/src/services/api.js
+++ data-storage-client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.https://mypersonalfile.onrender.com,
   withCredentials: true  // if you're sending cookies/token etc.
 });
 
